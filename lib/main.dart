beff6cd6a45d5eb2b76351ab663c0847bcb103cc
diaxenl <drboyd95@gmail.com>
2024-02-14 15:31:38 -0700

    Fixed dupe in main

--- lib/main.dart
+++ lib/main.dart
@@ -20,25 +20,3 @@ class TrainingApp extends StatelessWidget {
     );
   }
 }
-import 'package:flutter/material.dart';
-import 'training_home_page.dart'; // Import navigation.dart
-
-void main() {
-  runApp(const TrainingApp());
-}
-
-class TrainingApp extends StatelessWidget {
-  const TrainingApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Training App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:
-          const TrainingHomePage(), // Use TrainingHomePage from navigation.dart
-    );
-  }
-}
