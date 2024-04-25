f70cc79881fdcea201a4fa4d5554147f9e8af8fe
diaxenl <72827828+diaxenl@users.noreply.github.com>
2024-04-24 19:47:10 -0600

    updated dependencies and added a non-working version of the permission
    I don't have a tablet that can connect to Android Studio apparently so its gonna be annoying to test.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,20 @@ import 'standard_training_home_page.dart';
 import 'User_Stuff/user_login.dart';
 
 void main() {
+  /// This is generally what would be needed to get storage permissions but I don't know if the tablet
+  /// I got access to is the issue or something else. A nice post semester problem I dare say.
+  //  WidgetsFlutterBinding.ensureInitialized();
+  //
+  //   // Request essential permissions upfront
+  //   Map<Permission, PermissionStatus> statuses = await [
+  //     Permission.storage,
+  //     // Add other permissions as needed
+  //   ].request();
+  //
+  //   print("Permission statuses: $statuses");
+  //
+  //   runApp(const TrainingApp());
+
   runApp(const TrainingApp());
 }
 
